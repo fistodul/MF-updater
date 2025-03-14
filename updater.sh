7d406160a3c2eefdb48f1a0143fcad5aa7d4472d
filipopo <filipmymail@gmail.com>
2025-03-14 13:44:53 +0100

    Start ti update

--- updater.sh
+++ updater.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
 
+url='https://mf.nofisto.com/fast_download'
+
+skipFiles=(
+  #'Sounds/Announcer.uax'
+  #'Sounds/RagePlayerVoice.uax'
+  'System/Core.int'
+  'System/D3DDrv.int'
+  'System/Editor.int'
+  'System/Engine.int'
+  'System/Engine.u'
+  'System/Galaxy.int'
+  'System/IpDrv.int'
+  'System/IpServer.int'
+  'System/License.int'
+  'System/Manifest.int'
+  'System/MobileForcesEd.int'
+  'System/MobileForces.int'
+  'System/RageBrowser.int'
+  'System/RageGame.int'
+  'System/RageGfx.int'
+  'System/Rage.int'
+  'System/RageMenu.int'
+  'System/RageWeapons.int'
+  'System/Setup.int'
+  'System/Startup.int'
+  'System/UBrowser.int'
+  'System/Window.int'
+  'System/WinDrv.int'
+  #'Textures/rage_warehouse.utx'
+)
+
 # function arguments: $1 is the file to check the sum of
 sha_cmd () {
   # executes the injected command or defaults to shasum
@@ -24,174 +55,153 @@ grep_cmd () {
   eval "${GREP_CMD:-grep -oP '$1'}" < /dev/stdin
 }
 
-url='https://mf.nofisto.com/fast_download'
-
-# Get the shasums.txt file, or recreate it
-wget_cmd $url shasumss.txt ||
-curl_cmd "${url}/?C=M;O=D" | grep_cmd '(?<=href=").+?\.sha256' |
-  while read -r sha_file; do
-    curl_cmd "${url}/${sha_file}"
-  done > shasums.txt
-
-if ! [ -f shasums.txt ]; then
-  echo Failed to retrieve shasums.txt or recreate it
-  exit 1
-fi
-
-# Check files
-while read -r sha_file; do
-  echo $sha_file
-done < shasums.txt
-
-checkIfFilesExist()
-{
-  for folder in System Maps Textures Physics Sounds Music;
-  do
-    if [[ ! -d "../$folder" ]]; then
-      echo "Couldn't find $folder in parallel folders, can't continue";
-      read -n 1;
-      exit;
+checkIfFilesExist() {
+  for folder in System Maps Textures Physics Sounds Music; do
+    if ! [ -d "../$folder" ]; then
+      echo "Couldn't find $folder in parallel folders, can't continue"
+      read -n 1
+      exit 1
     fi
-  done;
+  done
 
-  if [[ ! -a "../System/UCC.exe" ]]; then
-    echo "Couldn't find UCC.exe in System folder, can't continue";
-    read -n 1;
-    exit;
+  if ! [ -f '../System/UCC.exe' ]; then
+    echo Couldn't find UCC.exe in System folder, can't continue
+    read -n 1
+    exit 1
   fi
 }
 
-downloadShasums()
-{
-  echo "Trying to download shasums.txt";
-  wget -q "https://mf.nofisto.com/fast_download/shasums.txt" -O "shasums.txt";
-  if (( $? == 0 )); then
-    echo "shasums.txt successfully downloaded";
-  else
-    echo "Failed to download shasums.txt";
-    read -n 1;
-    exit;
+downloadShasums() {
+  echo Trying to download sha512.txt
+
+  wget_cmd $url sha512.txt ||
+  curl_cmd "${url}/?C=M;O=D" | grep_cmd '(?<=href=").+?\.sha512' |
+    while read -r sha_file; do
+      curl_cmd "${url}/${sha_file}"
+    done > sha512.txt
+
+  if ! [ -f sha512.txt ]; then
+    echo Failed to download sha512.txt or recreate it
+    read -n 1
+    exit 1
   fi
 }
 
-setInfo()
-{
+setInfo() {
   if [[ -z $remoteHash ]]; then
-    remoteHash=$word;
-    return 1;
+    remoteHash=$word
+    return 1
+
   elif [[ -z $filename ]]; then
-    filename=$(echo $word | sed 's/.*\///');
-    ext=$(echo $filename | sed 's/.*\.//');
+    filename=$(echo $word | sed 's/.*\///')
+    ext=$(echo $filename | sed 's/.*\.//')
   fi
 }
 
-clearInfo()
-{
-  unset remoteHash;
-  unset filename;
+clearInfo() {
+  unset remoteHash
+  unset filename
 }
 
-recognizeExtension()
-{
+recognizeExtension() {
   case $ext in
     u)
-      folder='System';
+      folder='System'
       textFile=0;;
     umf)
-      folder='Maps';
+      folder='Maps'
       textFile=0;;
     int)
-      folder='System';
+      folder='System'
       textFile=1;;
     utx | usx)
-      folder='Textures';
+      folder='Textures'
       textFile=0;;
     uax)
-      folder='Sounds';
+      folder='Sounds'
       textFile=0;;
     umx)
-      folder='Music';
+      folder='Music'
       textFile=0;;
     COL | hnd2)
-      folder='Physics';
+      folder='Physics'
       textFile=1;;
     *)
-      echo Unknown extension. Aborting script.;
-      exit;;
-  esac;
+      echo "Unknown extension $ext, skipping";;
+  esac
 }
 
-setLocalHash()
-{
+setLocalHash() {
   if [[ -a "../${folder}/${filename}" ]]; then
-    localHash=$(sha256sum "../${folder}/${filename}" | sed 's/ .*//');
+    localHash=$(sha256sum "../${folder}/${filename}" | sed 's/ .*//')
   elif [[ ! -z $localHash ]]; then
-    unset localHash;
+    unset localHash
   fi
 }
 
-checkHashes()
-{
+checkHashes() {
   if [[ $localHash == $remoteHash ]]; then
-    echo "$filename is up to date";
-    clearInfo;
-    return 0;
+    echo "$filename is up to date"
+    clearInfo
+    return 0
+
   elif [[ -z $localHash ]]; then
-    echo "$filename is missing";
+    echo "$filename is missing"
   else
-    echo "$filename is mismatching";
-    echo "Local file hash:  $localHash";
-    echo "Remote file hash: $remoteHash";
+    echo "$filename is mismatching"
+    echo "Local file hash:  $localHash"
+    echo "Remote file hash: $remoteHash"
   fi
-  return 1;
+  return 1
 }
 
-getFile()
-{
+getFile() {
   if [[ $filename == 'Engine.u' || $filename == 'RageWeapons.u' ]]; then
-    echo "$filename is assumed to be indecompressible";
-    echo "Downloading $filename from the server";
-    wget -q -c "https://mf.nofisto.com/fast_download/$filename" -O "$filename";
-    mv "$filename" ../System;
+    echo "$filename is assumed to be indecompressible"
+    echo "Downloading $filename from the server"
+    wget -q -c "https://mf.nofisto.com/fast_download/$filename" -O "$filename"
+    mv "$filename" ../System
   elif (( ! $textFile )); then
-    echo "Downloading ${filename}.uz from the server";
-    wget -q -c "https://mf.nofisto.com/fast_download/${filename}.uz" -O "${filename}.uz";
-    echo "Decompressing ${filename}.uz";
-    cd "../System";
-    # ./UCC decompress "../Updater/${filename}.uz";
-    wine UCC decompress "../Updater/${filename}.uz" 2> /dev/null;
-    cd "../Updater";
+    echo "Downloading ${filename}.uz from the server"
+    wget -q -c "https://mf.nofisto.com/fast_download/${filename}.uz" -O "${filename}.uz"
+    echo "Decompressing ${filename}.uz"
+    cd "../System"
+    wine UCC decompress "../Updater/${filename}.uz" 2> /dev/null
+    cd "../Updater"
+
     if [[ $folder != 'System' ]]; then
-      mv "../System/$filename" "../$folder";
+      mv "../System/$filename" "../$folder"
     fi
+
     rm ${filename}.uz;
   else
-    echo "Downloading $filename from the server";
-    wget -q -c "https://mf.nofisto.com/fast_download/$filename" -O "$filename";
-    mv "$filename" "../$folder";
+    echo "Downloading $filename from the server"
+    wget -q -c "https://mf.nofisto.com/fast_download/$filename" -O "$filename"
+    mv "$filename" "../$folder"
   fi
 }
 
-checkIfFilesExist;
-downloadShasums;
+checkIfFilesExist
+downloadShasums
 
-for word in $(cat shasums.txt);
-do
-  setInfo;
+while read word; do
+  setInfo
+
   if (( $? == 1 )); then
-    continue;
+    continue
   fi
-  recognizeExtension;
-  setLocalHash;
-  checkHashes;
+
+  recognizeExtension
+  setLocalHash
+  checkHashes
+
   if (( $? == 0 )); then
-    continue;
+    continue
   fi
-  getFile;
-  clearInfo;
-done;
 
-echo 'Update finished';
-read -n 1;
+  getFile
+  clearInfo
+done < sha512.txt
 
-exit;
+echo Update finished
+read -n 1
