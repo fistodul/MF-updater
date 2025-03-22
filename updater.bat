e70cfc76818aff063390a3bf244c20a6cea82b61
filipopo <filipmymail@gmail.com>
2025-03-22 13:26:31 +0100

    Prepare to test on Windows

--- updater.bat
+++ updater.bat
@@ -5,6 +5,6 @@ rem https://github.com/git-for-windows/git/releases/tag/v2.46.2.windows.1
 
 set SHA_CMD=certutil -hashfile $1 SHA512
 set WGET_CMD=certutil -urlcache -f "$1/$2" $2
-set WINE_CMD=start
+set WINE_CMD=start $*
 
-sh.exe updater.sh
+bash.exe updater.sh
